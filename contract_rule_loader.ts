42a5202ded112e1431f87495e681d577292a329a
Aptymok <115195341+Aptymok@users.noreply.github.com>
2026-05-21 11:19:46 -0600

    feat: add contract rule loader

---
+++ contract_rule_loader.ts
@@ -0,0 +1,10 @@
+export function loadContractRules() {
+  return [
+    {
+      id: '1',
+      contractCode: 'CA_10_CGI_2019',
+      ruleName: 'REPARACION_EN_SITIO',
+      limitValue: 2
+    }
+  ]
+}
