127db624ca7ca07a56253adbb5c5d513eb3e071e
Aptymok <115195341+Aptymok@users.noreply.github.com>
2026-05-21 12:12:53 -0600

    phase2: route main entry through src ui

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './ui_app.js'
+import { App } from './src/ui/App.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   React.createElement(App)
